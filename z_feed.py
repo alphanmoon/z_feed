1bd0f30faa0c9765ef08855093f0ee0b26f8ef21
alphanmoon <aethereus@gmail.com>
2017-05-30 10:16:51 +0300

    Update z_feed.py

--- z_feed.py
+++ z_feed.py
@@ -1,50 +1,52 @@
-# all prints() are used in console testing, delete/comment while in production   
+## All prints() are used for console testing, delete/comment while in production 
 
-import feedparser, nltk, tweepy, json, time
+import feedparser, nltk, tweepy, json, random, time
 from pyshorteners import Shortener
 
-used_file = 'used_links_list.json'
-
-#Erasing [used links] in json in testing mode, comment when no need
-used_links = []
-with open(used_file, 'w') as used:
-    json.dump(used_links, used)
-
-# Insert rss feed url
-feed = ['https://foo.bar.foo.bar', \
-        'https://foo.bar.foo.bar', \
-        'https://foo.bar.foo.bar',
-        ]
+#Erasing [used links] in json for console testing, comment if no need
+##used_links = []
+##with open(used_file, 'w') as used:
+##    json.dump(used_links, used)
 
 #Parsing a news title by iterating news feeds
 while True:
-    #Parsing news feeds    
-    for k in range(len(feed)):    
-        d = feedparser.parse(feed[k])
-        # TODO: COUNT TITLES IN FEED
-        n_titles = len(d['entries'])
-        for i in range(n_titles):    # SOME FEEDS ARE LESS THAN fixed no of TITLES CAUSING ERROR !!!!! hence COUNT TITLES IN FEEDS
 
+    used_file = 'used_links_list_test.json'
+
+    feed = ['https://foo.bar.foo.bar',
+        'https://foo.bar.foo.bar',
+        'https://foo.bar.foo.bar',
+        ]
+    
+    # reloading feed list
+    feed_run = feed
+    
+    while feed_run:
+        # Randomly selecting then parsing feed
+        select_feed = random.choice(feed_run)        
+        d = feedparser.parse(select_feed)
+
+        # Removing selected feed from actual run list
+        feed_run.remove(select_feed)
+        
+        n_titles = len(d['entries'])  # COUNTING TITLES IN FEED
+        
+        # Iterating titles till news occurs, if yes - tweeting
+        for i in range(n_titles):
+            
+            #Parsing a link from title                    
+            url = d.entries[i].link  
+            
             # Loading json with used links
             with open(used_file) as used:
                 used_links = json.load(used)
-
-            #Parsing a link from title                    
-            url = d.entries[i].link
-
-            # Adding used 'link' to a json list to exclude used news    
-##            with open(used_file) as open_json:
-##                links_old = json.load(open_json)
+            
             if url not in used_links:
-                used_links.append(url)
-                with open(used_file, 'w') as used:
-                    json.dump(used_links, used)
-                    print('dump saved')
 
                 # Parsing title        
                 post1 = d.entries[i].title
-
-                # Inserting hashtags afore title nouns;  ******magic******
+                    
+                # Inserting hashtags in title nouns;  ******tags magic******
                 toks = nltk.word_tokenize(post1)
                 tags = nltk.pos_tag(toks)
                 # 1stly make list from tuples
@@ -52,30 +54,27 @@ while True:
                 for t in tags_list:
                     if t[1] in ['NN', 'NNP']:                    
                         t[0] = '#' + t[0]
-                    t[1] = ''
-                   
-                post2 = ' '.join([''.join(elem) for elem in tags_list])
-                #print(post2)    
+                    t[1] = ''                   
+                post2 = ' '.join([''.join(elem) for elem in tags_list])                      
 
                 # Shortening url
                 while True:
-                    api_key = 'api_key'
+                    api_key = 'AIzaSyCZfjCR6iSq6lfCsYCkNqeSBzChhuM9iKY'
                     shortener = Shortener('Google', api_key=api_key)
                     short_link = shortener.short(url)
                     if short_link:
-                        print('link OK')
                         break
 
                 #Building final post  
                 post3 = post2 + ' ' + short_link
-                if 139 > len(post3) > 40:                
-                    print('post composed, waitin 10 sec then tweeting:', post3)
+                if 139 > len(post3) > 40:
 
-                    time.sleep(10)
-    
-                                          
+                    rsleep = random.randint(200,600)                    
+                    print('post is built, waiting %s sec' % rsleep)
+                    time.sleep(rsleep)
+                                      
                     #Tweeting
-                    while True:
+                    while True:                       
                         try:
                             CONSUMER_KEY = 'YOUR_KEY'
                             CONSUMER_SECRET = 'CONSUMER_SECRET'
@@ -86,15 +85,18 @@ while True:
                             api = tweepy.API(auth)
                             tweeted = api.update_status(post3)
                             if tweeted:
-                                print('tweeted!')
+                                print('tweeted! %s from %s' % (post3, select_feed))                        
                                 break
                         except:
-                            print('tweepy error, sleepin 3 min')
+                            print('tweepy error')
                             time.sleep(180)
-
-    else:
-        print('no news')    
-                                           
-    print('Waiting 5 sec for the next cycle') 
-    time.sleep(5)
-
+                            
+                    # Saving used links in json
+                    with open(used_file, 'w') as used:
+                        used_links.append(url)
+                        json.dump(used_links, used)
+                        print('dump is OK')
+                    break
+                                
+    print('reloading feed list in n sec for the next random run')
+    time.sleep(30)
